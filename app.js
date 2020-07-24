ae33eb6e743840956168b2cd91d46fbb946939b7
mt <margarettom6@gmail.com>
2020-07-24 11:06:15 +0300

    Implemented a .get

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const router = express.Router();
 
 router.route('/habits')
-  .get()
+  .get((req, res) => {
+    const response = 'My api';
+    return res.json(response);
+  })
   .post();
 
-app.get('/', (req, res) => {
-  res.send('Habit Tracker API');
-});
+app.use('/habittracker', router);
+
+// app.get('/habits', (req, res) => {
+//   res.send('Habit Tracker API');
+// });
 
 app.listen(port, () => {
   console.log(`listening on Port ${port}`);
