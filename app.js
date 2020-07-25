6d558992a88f5779fe1095735a6a2161fed5d5a0
mt <margarettom6@gmail.com>
2020-07-25 22:47:25 +0300

    set up Mongoose

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const mongoose = require ('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
 const router = express.Router();
+const db = mongoose.connect('mongodb://localhost/habittracker');
+const Habit = require('./models/habitModel');
 
 router.route('/habits')
   .get((req, res) => {
