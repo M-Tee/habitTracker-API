b9989d67ec6c646455640fe19ce472697e67309b
mt <margarettom6@gmail.com>
2020-07-30 14:57:59 +0300

    Rendering data & html elements using the DOM

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const container = document.getElementById('root');
 
-
 (async () => {
   const response = await axios({
     url: 'http://localhost:4000/habittracker/habits',
@@ -9,23 +8,33 @@ const container = document.getElementById('root');
     .then(response => {
       let data = response.data;
 
+      //loop through the data
       data.forEach(habit => {
+        //Create element, set attributes and append element
         const card = document.createElement('div')
         card.setAttribute('class', 'card')
         container.appendChild(card)
 
+        const content = document.createElement('div')
+        content.setAttribute('class', 'content')
+        card.appendChild(content)
+
         const input = document.createElement('input')
         input.setAttribute('type', 'checkbox')
-        card.appendChild(input)
-  
+        content.appendChild(input)
+
         const p = document.createElement('p')
         p.setAttribute('class', 'habit-title')
         p.textContent = habit.title
-        card.appendChild(p)
+        content.appendChild(p)
 
-        // const editIcon = document.createElement('i');
-        // editIcon.setAttribute('class','fa fa-check-circle-o');
-        // card.appendChild(editIcon)
+        const edit = document.createElement('div')
+        edit.setAttribute('class', 'edit')
+        card.appendChild(edit)
+
+        edit.insertAdjacentHTML('beforeend', '<p class="iconify" data-icon="clarity:edit-solid" data-inline="false" style="color: purple;"></p>');
+        edit.insertAdjacentHTML('beforeend', '<p class="iconify" data-icon="carbon:delete" data-inline="false" style="color: purple;"></p>');
+
       })
     })
     .catch(err => {
