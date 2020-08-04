12d23afd34c498eaa04913da378ba591e929bccd
mt <margarettom6@gmail.com>
2020-08-04 09:22:59 +0300

    Index.js Code refactoring and clean up

--- index.js
+++ index.js
@@ -4,110 +4,68 @@ let displaySavedHabits = async function () {
   const response = await axios({
     url: 'http://localhost:4000/habittracker/habits',
     method: 'get'
-  }) 
-  .then(response => {
-    let data = response.data;
-
-    //loop through the data
-    data.forEach(habit => {
-      //Create element, set attributes and append element
-      const card = document.createElement('div')
-      card.setAttribute('class', 'card')
-      container.appendChild(card)
-
-      const content = document.createElement('div')
-      content.setAttribute('class', 'content')
-      card.appendChild(content)
-
-      const input = document.createElement('input')
-      input.setAttribute('type', 'checkbox')
-      content.appendChild(input)
-
-      const p = document.createElement('p')
-      p.setAttribute('class', 'habit-title')
-      p.textContent = habit.title
-      content.appendChild(p)
-
-      const edit = document.createElement('div')
-      edit.setAttribute('class', 'edit')
-      card.appendChild(edit)
-
-      edit.insertAdjacentHTML('beforeend', '<p class="iconify" data-icon="clarity:edit-solid" data-inline="false" style="color: purple;"></p>');
-      edit.insertAdjacentHTML('beforeend', '<p class="iconify" data-icon="carbon:delete" data-inline="false" style="color: purple;"></p>');
-    })
   })
-  .catch(err => {
-    console.log(err)
-  });
-}
+    .then(response => {
+      let data = response.data;
 
+      //loop through the data
+      data.forEach(habit => {
+        //Create element, set attributes and append element
+        const card = document.createElement('div')
+        card.setAttribute('class', 'card')
+        container.appendChild(card)
+
+        const content = document.createElement('div')
+        content.setAttribute('class', 'content')
+        card.appendChild(content)
+
+        const input = document.createElement('input')
+        input.setAttribute('type', 'checkbox')
+        content.appendChild(input)
+
+        const p = document.createElement('p')
+        p.setAttribute('class', 'habit-title')
+        p.textContent = habit.title
+        content.appendChild(p)
+
+        const edit = document.createElement('div')
+        edit.setAttribute('class', 'edit')
+        card.appendChild(edit)
+
+        edit.insertAdjacentHTML('beforeend', '<p class="iconify" data-icon="clarity:edit-solid" data-inline="false" style="color: purple;"></p>');
+        edit.insertAdjacentHTML('beforeend', '<p class="iconify" data-icon="carbon:delete" data-inline="false" style="color: purple;"></p>');
+      })
+    })
+    .catch(err => {
+      console.log(err)
+    });
+}
 displaySavedHabits()
 
-let form = document.getElementById('form');
+const popup = document.querySelector('.popup');
+const openPopup = document.getElementById('add')
+  .addEventListener('click', function () {
+    popup.style.display = "flex";
+  });
 
-form.addEventListener('submit',async (event) => {
-  event.preventDefault();
+const closePopup = document.querySelector('.close')
+  .addEventListener('click', function () {
+    popup.style.display = "none";
+  });
 
-  const title = document.getElementById('habit').value;
-  const description = document.getElementById('description').value;
+const form = document.getElementById('form');
 
-  const habit = { title, description}
+let addHabit = form.addEventListener('submit', async (event) => {
+    event.preventDefault();
 
-  await axios.post('http://localhost:4000/habittracker/habits', habit)
+    const title = document.getElementById('habit').value;
+    const description = document.getElementById('description').value;
 
-  .then(
-    displaySavedHabits()
-  )
-})
+    const habit = { title, description }
 
+    await axios.post('http://localhost:4000/habittracker/habits', habit)
 
-
-// (async () => {
-//   const response = await axios({
-//     url: 'http://localhost:4000/habittracker/habits',
-//     method: 'get'
-//   })
-//     .then(response => {
-//       let data = response.data;
-
-//       //loop through the data
-//       data.forEach(habit => {
-//         //Create element, set attributes and append element
-//         const card = document.createElement('div')
-//         card.setAttribute('class', 'card')
-//         container.appendChild(card)
-
-//         const content = document.createElement('div')
-//         content.setAttribute('class', 'content')
-//         card.appendChild(content)
-
-//         const input = document.createElement('input')
-//         input.setAttribute('type', 'checkbox')
-//         content.appendChild(input)
-
-//         const p = document.createElement('p')
-//         p.setAttribute('class', 'habit-title')
-//         p.textContent = habit.title
-//         content.appendChild(p)
-
-//         const edit = document.createElement('div')
-//         edit.setAttribute('class', 'edit')
-//         card.appendChild(edit)
-
-//         edit.insertAdjacentHTML('beforeend', '<p class="iconify" data-icon="clarity:edit-solid" data-inline="false" style="color: purple;"></p>');
-//         edit.insertAdjacentHTML('beforeend', '<p class="iconify" data-icon="carbon:delete" data-inline="false" style="color: purple;"></p>');
-
-//       })
-//     })
-//     .catch(err => {
-//       console.log(err)
-//     })
-// })()
-
-document.getElementById('add').addEventListener('click', function () {
-  document.querySelector('.popup').style.display = "flex";
-});
-
-document.querySelector('.close').addEventListener('click', function () {
-  document.querySelector('.popup').style.display = "none";
-});
+      .then(
+        displaySavedHabits()
+      )
+  })
