a313f3d7ff99772ebd5435900896e22cae9ee584
mt <margarettom6@gmail.com>
2020-08-04 09:11:55 +0300

    Function to display saved habits

--- index.js
+++ index.js
@@ -1,51 +1,96 @@
 const container = document.getElementById('root');
 
-(async () => {
+let displaySavedHabits = async function () {
   const response = await axios({
     url: 'http://localhost:4000/habittracker/habits',
     method: 'get'
+  }) 
+  .then(response => {
+    let data = response.data;
+
+    //loop through the data
+    data.forEach(habit => {
+      //Create element, set attributes and append element
+      const card = document.createElement('div')
+      card.setAttribute('class', 'card')
+      container.appendChild(card)
+
+      const content = document.createElement('div')
+      content.setAttribute('class', 'content')
+      card.appendChild(content)
+
+      const input = document.createElement('input')
+      input.setAttribute('type', 'checkbox')
+      content.appendChild(input)
+
+      const p = document.createElement('p')
+      p.setAttribute('class', 'habit-title')
+      p.textContent = habit.title
+      content.appendChild(p)
+
+      const edit = document.createElement('div')
+      edit.setAttribute('class', 'edit')
+      card.appendChild(edit)
+
+      edit.insertAdjacentHTML('beforeend', '<p class="iconify" data-icon="clarity:edit-solid" data-inline="false" style="color: purple;"></p>');
+      edit.insertAdjacentHTML('beforeend', '<p class="iconify" data-icon="carbon:delete" data-inline="false" style="color: purple;"></p>');
+    })
   })
-    .then(response => {
-      let data = response.data;
+  .catch(err => {
+    console.log(err)
+  });
+}
 
-      //loop through the data
-      data.forEach(habit => {
-        //Create element, set attributes and append element
-        const card = document.createElement('div')
-        card.setAttribute('class', 'card')
-        container.appendChild(card)
+displaySavedHabits()
 
-        const content = document.createElement('div')
-        content.setAttribute('class', 'content')
-        card.appendChild(content)
 
-        const input = document.createElement('input')
-        input.setAttribute('type', 'checkbox')
-        content.appendChild(input)
 
-        const p = document.createElement('p')
-        p.setAttribute('class', 'habit-title')
-        p.textContent = habit.title
-        content.appendChild(p)
+// (async () => {
+//   const response = await axios({
+//     url: 'http://localhost:4000/habittracker/habits',
+//     method: 'get'
+//   })
+//     .then(response => {
+//       let data = response.data;
 
-        const edit = document.createElement('div')
-        edit.setAttribute('class', 'edit')
-        card.appendChild(edit)
+//       //loop through the data
+//       data.forEach(habit => {
+//         //Create element, set attributes and append element
+//         const card = document.createElement('div')
+//         card.setAttribute('class', 'card')
+//         container.appendChild(card)
 
-        edit.insertAdjacentHTML('beforeend', '<p class="iconify" data-icon="clarity:edit-solid" data-inline="false" style="color: purple;"></p>');
-        edit.insertAdjacentHTML('beforeend', '<p class="iconify" data-icon="carbon:delete" data-inline="false" style="color: purple;"></p>');
+//         const content = document.createElement('div')
+//         content.setAttribute('class', 'content')
+//         card.appendChild(content)
 
-      })
-    })
-    .catch(err => {
-      console.log(err)
-    })
-})()
+//         const input = document.createElement('input')
+//         input.setAttribute('type', 'checkbox')
+//         content.appendChild(input)
 
-document.getElementById('add').addEventListener('click', function(){
+//         const p = document.createElement('p')
+//         p.setAttribute('class', 'habit-title')
+//         p.textContent = habit.title
+//         content.appendChild(p)
+
+//         const edit = document.createElement('div')
+//         edit.setAttribute('class', 'edit')
+//         card.appendChild(edit)
+
+//         edit.insertAdjacentHTML('beforeend', '<p class="iconify" data-icon="clarity:edit-solid" data-inline="false" style="color: purple;"></p>');
+//         edit.insertAdjacentHTML('beforeend', '<p class="iconify" data-icon="carbon:delete" data-inline="false" style="color: purple;"></p>');
+
+//       })
+//     })
+//     .catch(err => {
+//       console.log(err)
+//     })
+// })()
+
+document.getElementById('add').addEventListener('click', function () {
   document.querySelector('.popup').style.display = "flex";
 });
 
-document.querySelector('.close').addEventListener('click', function(){
+document.querySelector('.close').addEventListener('click', function () {
   document.querySelector('.popup').style.display = "none";
 });
